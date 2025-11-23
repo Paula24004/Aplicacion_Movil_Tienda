b58e0161bf11ef92879138f07e5a2e188d8854d9
carl287 <ca.calvio@duocuc.cl>
2025-11-23 19:56:24 -0300

    arregle problema al iniciar app

--- app/src/main/java/com/example/apptiendadeportiva_grupo10/MainActivity.kt
+++ app/src/main/java/com/example/apptiendadeportiva_grupo10/MainActivity.kt
@@ -4,41 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.remember
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.example.apptiendadeportiva_grupo10.ui.screens.CatalogoScreen
-import com.example.apptiendadeportiva_grupo10.ui.screens.DetalleProductoScreen
-import com.example.apptiendadeportiva_grupo10.viewmodel.CatalogoViewModel
-import com.example.apptiendadeportiva_grupo10.viewmodel.CarritoViewModel
+import com.example.apptiendadeportiva_grupo10.navigation.RootScreen
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val navController = rememberNavController()
-            val viewModel = remember { CatalogoViewModel() }
-            val carritoViewModel = remember { CarritoViewModel() }
             Surface {
-                NavHost(navController = navController, startDestination = "catalogo") {
-                    composable("catalogo") {
-                        CatalogoScreen(
-                            navController = navController,
-                            viewModel = viewModel,
-                            carritoViewModel = carritoViewModel
-                        )
-                    }
-                    composable("detalle/{productoId}") { back ->
-                        val id = back.arguments?.getString("productoId")?.toIntOrNull() ?: -1
-                        DetalleProductoScreen(
-                            navController = navController,  // Agrega esto
-                            productoId = id,
-                            viewModel = viewModel,
-                            carritoViewModel = carritoViewModel
-                        )
+                RootScreen()
                     }
                 }
             }
         }
-    }
-}
